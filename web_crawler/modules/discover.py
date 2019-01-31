9325ad67f57ee99ff3222269cfd8a1a6708a28d9
Damian <37978110+dszyszek@users.noreply.github.com>
2019-01-31 14:11:03 +0100

    Add ability to search for sub-domains

--- web_crawler/modules/discover.py
+++ web_crawler/modules/discover.py
@@ -3,17 +3,30 @@ import requests
 
 
 def discover():
-    print('\n=================================================================\n')
+    print('\n=================================================================')
     print('Discover mode')
-    print('\n=================================================================\n')
+    print('=================================================================\n')
 
-    page = input('Enter url address:\n')
+    page = input('Enter name of the page:\n')
+    dic = input('\nEnter name of your searching-dictionary (if you don\'t have one, just type pre)\n')
 
-    find(page)
+    find(page, dic)
 
 
 
 
-def find(url):
-    a = requests.get(url)
-    print(a.text)
+def find(url, log_file):
+
+    with open(f'./dict/{log_file}.log', 'r') as file:
+        try:
+            for line in file:
+                single_line = line.strip()
+                req = requests.get(f'http://{single_line}.{url}')
+
+                if req:
+                    print(f'Found sub-domain! www.{single_line}.{url}')
+        except:
+            print('ERROR! Cannot do that')
+
+    # a = requests.get(url)
+    # print(a.text)
