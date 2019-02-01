6eb2a8c75cc87e9b35c3982bc1de022fa2a4e8a7
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-01 22:13:27 +0100

    Add basic crawling ability to site_map

--- web_crawler/modules/crawl.py
+++ web_crawler/modules/crawl.py
@@ -1,4 +1,8 @@
 import re
+from urllib.parse import urljoin
+import requests
+
+target = []
 
 
 def crawl():
@@ -8,7 +12,26 @@ def crawl():
 
     page = input('Enter name of the page:\n')
 
+    if not 'http://' or not 'https://' in page:
+        page = f'http://{page}'
+
     site_map(page)
 
+
+def get_links(url):
+    res = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', res.text)
+
+
 def site_map(url):
-    print(url)
+    links = get_links(url)
+
+    # print(links)
+
+    for link in links:
+        link = urljoin(url, link)
+
+        if url in link and link not in target:
+            target.append(link)
+            print(link)
+            site_map(link)
