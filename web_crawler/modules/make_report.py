0177aa2ae976dca151b8a7ffd9b3e42470c39073
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-01 01:30:19 +0100

    Add function that makes reports

---
+++ web_crawler/modules/make_report.py
@@ -0,0 +1,10 @@
+import datetime
+
+def make_report(url, results, dir):
+    now = datetime.datetime.now()
+    with open(f'./reports/{dir}/report_{now.strftime("%Y-%m-%d")}_{now.strftime("%H:%M")}', 'w+') as file:
+        file.write(f'================= Report {now.strftime("%Y-%m-%d %H:%M")} =================\n')
+        file.write(f'Domain: http://{url}')
+        file.write('\nSubdomains:\n')
+        for i,x in enumerate(results):
+            file.write(f'{i+1}) {x}\n')
