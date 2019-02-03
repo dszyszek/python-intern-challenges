6bf54e314ac4f43573a0730fd9d8b0f88ef02e03
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-03 16:25:59 +0100

    Add description to functions

--- csv_report_processing/modules/process_file.py
+++ csv_report_processing/modules/process_file.py
@@ -5,7 +5,7 @@ import requests
 
 
 def process_file(file):
-
+    '''This function will init processing given .csv file'''
     with open(file) as csv_file:
         file_read = reader(csv_file)
 
@@ -14,27 +14,32 @@ def process_file(file):
 
 
 def change_date_format(date_string):
-    date = datetime.strptime(date_string, '%m/%d/%Y')
-    proper_date = datetime.strftime(date, '%Y-%m-%d')
+    '''This function will change form of given date into required in task'''
+    date = datetime.strptime(date_string, '%m/%d/%Y')       # Change date string into datetime object
+    proper_date = datetime.strftime(date, '%Y-%m-%d')       # Change date formatting into required one
 
     return proper_date
 
 
 def get_country_shortcut(city_name):
+    '''This function will try to find three-letter country code of country, in which given city is located'''
+    country_code = 'XXX'
 
-    country_code = ''
+    try:
+        for c in list(pycountry.subdivisions):      # Extract two-letter country code from name of city
+            if c.name == city_name:
+                country_code = c.country_code
 
-    for c in list(pycountry.subdivisions):
-        if c.name == city_name:
-            country_code = c.country_code
-
-    for b in list(pycountry.countries):
-        if b.alpha_2 == country_code:
-            print(b.alpha_3)
+        for b in list(pycountry.countries):         # change extracted two-letter country code to three-letter one
+            if b.alpha_2 == country_code:
+                return b.alpha_3
+    except:
+        return country_code
 
 
 
 # get_country = input('Input name of the city:\n')
-# get_country_shortcut(get_country)
+# res = get_country_shortcut(get_country)
+# print(res)
 
 # get_country_shortcut('Encamp')
