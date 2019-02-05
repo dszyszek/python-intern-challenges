10245ab55138ac59188fff502b95df1258831380
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-05 12:39:18 +0100

    Test adding utf-16 files

--- csv_report_processing/tests/tests.py
+++ csv_report_processing/tests/tests.py
@@ -9,19 +9,24 @@ from modules.process_file import process_file
 
 class ProcessFileTests(unittest.TestCase):
 
-    def test_file_creation(self):
-        """Test if process_file() will create file while given valid file in input"""
+    def test_file_creation_utf_8(self):
+        """Test if process_file() will create file while given valid file in input (utf-8 input)"""
         process_file('./test_files/correct.csv', 'test_correct')
 
         self.assertTrue(os.path.isfile('../processed_files/test_correct.csv'))
-        os.remove(f'{os.getcwd()}/../processed_files/test_correct.csv')
+
+    def test_file_creation_utf_16(self):
+        """Test if process_file() will create file while given valid file in input (utf-16 input)"""
+        process_file('./test_files/utf16_file.csv', 'test_correct')
+
+        self.assertTrue(os.path.isfile('../processed_files/test_correct.csv'))
 
     def test_wrong_format(self):
         """Test if function will stop in case if file with wrong format (in this case != .csv) passed"""
         file = process_file('./test_files/wrong_file_format.log', 'test_correct')
 
         self.assertFalse(file)
-        os.remove(f'{os.getcwd()}/../processed_files/test_correct.csv')
+        os.remove(f'{os.getcwd()}/../processed_files/test_correct.csv')     # Remove created test_correct.csv file
 
     def test_empty_file(self):
         """Test if process_file() will break after passing empty file"""
