fc93ff37b4af06b3fa7e5e649508b50113372149
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-05 15:18:09 +0100

    Fasten making requests

--- web_crawler/modules/discover.py
+++ web_crawler/modules/discover.py
@@ -24,7 +24,7 @@ def find(url, log_file):
             print(f'Testing: {single_line}.{url}')
 
             try:
-                req = requests.get(f'http://{single_line}.{url}', timeout=2)
+                req = requests.get(f'http://{single_line}.{url}', timeout=(1, 0.5))
                 if req:
                     found.append(f'{single_line}.{url}')
 
